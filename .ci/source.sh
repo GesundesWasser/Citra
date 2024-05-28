ddc5acebc07c2a50e1f0c27824a526de5bb7f2d4
GesundesWasser <gesundeswasser@duck.com>
2024-05-28 21:37:53 +0200

    Missing Repo's Fix
    * Get The GameDB From Gist
    * Fix Missing Submodules

--- .ci/source.sh
+++ .ci/source.sh
@@ -9,7 +9,7 @@ COMPAT_LIST='dist/compatibility_list/compatibility_list.json'
 mkdir artifacts
 
 pip3 install git-archive-all
-wget -q https://api.citra-emu.org/gamedb -O "${COMPAT_LIST}"
+wget -q https://gist.github.com/GesundesWasser/f17f9286fcb666c14ca9c9dafc45f67f/raw/6b904a96e911135884cb9f361259de01b7238043/gamedb.json -O "${COMPAT_LIST}"
 git describe --abbrev=0 --always HEAD > GIT-COMMIT
 git describe --tags HEAD > GIT-TAG || echo 'unknown' > GIT-TAG
 git archive-all --include "${COMPAT_LIST}" --include GIT-COMMIT --include GIT-TAG --force-submodules artifacts/"${REV_NAME}.tar"
